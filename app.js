e32de21cb34bf0b88c8d8994ef91594703aa0e5f
willitcode <91231142+willitcode@users.noreply.github.com>
2024-04-16 22:47:59 -0500

    remove random comment in js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ function setSlider() {
     var params = new URLSearchParams(window.location.search);
     if (params.get("v") != null) {
         document.getElementById("slider").value = params.get("v");
-        /* can't decide if I should disable the slider or not... I don't think I should */
     };
 };
 
